b1e40eb41a29b35a2ba911bfd8d5344454513f73
Shreenidhi Bharadwaj HK <shreenidhihk@outlook.com>
2022-08-31 23:18:38 +0530

    added

--- src/NCD/NCD_Builder.groovy
+++ src/NCD/NCD_Builder.groovy
@@ -1,20 +1,25 @@
-package NCD 
+package NCD
 
 class NCD_Builder {
 
-    static int works= 604
+    static int works = 604
 
     public static void main(String[] args) {
         println('Hello ')
     }
 
-    public static getMethod(){
-        return works;
+    public static getMethod() {
+        return works
     }
 
-    public static String getResources(Script script){
+    public static String getResources(Script script) {
         String string = script.libraryResource 'text.txt'
+        string = hello()
         return string
+    }
+
+    private String hello(){
+        return 'Hello'
     }
 
 }
