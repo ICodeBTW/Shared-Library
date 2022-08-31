74802f9b00b1e92877f9f7c3c1016b2274287dfc
Shreenidhi Bharadwaj HK <shreenidhihk@outlook.com>
2022-08-31 22:38:03 +0530

    added

--- vars/called.groovy
+++ vars/called.groovy
@@ -4,6 +4,6 @@ def call(Script script) {
     println('Hello world!')
     script.stage('new Stage') {
         def number = NCD_Builder.getMethod()
-        echo number
+        println(number)
     }
 }
