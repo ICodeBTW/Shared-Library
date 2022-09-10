8c4f66776e343cfa25ecd8731b228b0f6488c707
Shreenidhi Bharadwaj HK <shreenidhihk@outlook.com>
2022-09-10 19:42:22 +0530

    Updated

--- vars/called.groovy
+++ vars/called.groovy
@@ -3,7 +3,7 @@ import com.company.build.Builder;
 def call(Script script) {
     println('Hello world!')
     script.stage('new Stage') {
-        Builder instance = Builder.getInstance();
+        def instance = Builder.getInstance();
         instance.echo()
 
     }
